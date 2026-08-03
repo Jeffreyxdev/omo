86da9a22ac96e3f454ffb4b243f9706853293256
Jeffrey <agabaenwerejeffrey@gmail.com>
2026-08-03 19:45:11 +0100

    vv

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@udyking/db", "@udyking/shared"],
+  outputFileTracingRoot: require("path").resolve(process.cwd(), "../.."),
 };
 
 export default nextConfig;
